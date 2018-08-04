0e4f339ebb164a18bd6478564b2268f85738e763
aeffiong <aeffiong9@gmail.com>
2018-08-04 12:46:00 -0600

    working on api routes and verifying sequelize models are working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,46 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all appointments
+  app.get("/api/appointments", function(req, res) {
+    db.Appointments.findAll({}).then(function(dbAppointments) {
+      res.json(dbAppointments);
+    });
+  });
+  // Get all clients
+  app.get("/api/clients", function(req, res) {
+    db.Clients.findAll({}).then(function(dbClients) {
+      res.json(dbClients);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Create a new appointment
+  app.post("/api/appointments", function(req, res) {
+    db.Appointments.create(req.body).then(function(dbAppointments) {
+      res.json(dbAppointments);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+   // Create a new client
+   app.post("/api/clients", function(req, res) {
+    db.Clients.create(req.body).then(function(dbClients) {
+      res.json(dbClients);
+    });
+  });
+
+  // Delete an appointment by id
+  app.delete("/api/appointments/:id", function(req, res) {
+    db.Appointments.destroy({ where: { id: req.params.id } }).then(function(dbAppointments) {
+      res.json(dbAppointments);
+    });
+  });
+
+  // Delete an appointment by id
+  app.delete("/api/clients/:id", function(req, res) {
+    db.Appointments.destroy({ where: { id: req.params.id } }).then(function(dbClients) {
+      res.json(dbClients);
     });
   });
 };
+
+// will create the rest, but thinking they might want to be in their own .js files to keep things in order
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,10 +3,10 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Appointments.findAll({}).then(function(dbAppointments) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        examples: dbAppointments
       });
     });
   });
