f1d0f6c2b5b21f168f1955609833c0b21a917ec0
Trevis Thomas <trevislynnthomas@gmail.com>
2018-08-09 20:36:08 -0600

    I don't even know.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,36 +1,40 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Appointments.findAll({}).then(function(dbAppointments) {
-      res.sendFile(path.join(__dirname, "../public/index.html"));
+  app.get("/", function (req, res) {
+    db.Appointments.findAll({}).then(function (dbAppointments) {
+      res.render("index");
     });
   });
 
-    // Load signup page
-    app.get("/signup", function(req, res) {
-      db.Appointments.findAll({}).then(function(dbAppointments) {
-        res.sendFile(path.join(__dirname, "../public/signup.html"));
-      });
+  // Load signup page
+  app.get("/signup", function (req, res) {
+    db.Appointments.findAll({}).then(function (dbAppointments) {
+      res.render("signup");
     });
+  });
 
-    // load dashboard page
-    app.get("/dashboard", function(req, res) {
-      db.Clients.findAll({}).then(function(dbClients) {
-        res.sendFile(path.join(__dirname, "../public/dashboard.html"));
+  // load dashboard page
+  app.get("/dashboard", function (req, res) {
+    db.Clients.findAll({}).then(function (dbClients) {
+      res.render("dashboard", {
+        msg: "hi"
       });
     });
+  });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Clients.findOne({ where: { id: req.params.id } }).then(function(dbClients) {
-      res.sendFile(path.join(__dirname, "../public/404.html"));
+  app.get("/example/:id", function (req, res) {
+    db.Clients.findOne({ where: { id: req.params.id } }).then(function (dbClients) {
+      res.render("example", {
+        example: dbClients
+      });
     });
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/404.html"));
+  app.get("*", function (req, res) {
+    res.render("404");
   });
 };
