1983ab101c0caa04fde6ff74835590118c3518f2
aeffiong <aeffiong9@gmail.com>
2018-08-09 21:29:57 -0600

    html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,40 +1,36 @@
 var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Appointments.findAll({}).then(function(dbAppointments) {
-      res.render("index");
+      res.sendFile(path.join(__dirname, "../public/index.html"));
     });
-  });
+  
 
     // Load signup page
     app.get("/signup", function(req, res) {
-      db.Appointments.findAll({}).then(function(dbAppointments) {
-        res.render("signup");
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
       });
-    });
+
 
     // load dashboard page
     app.get("/dashboard", function(req, res) {
-      db.Clients.findAll({}).then(function(dbClients) {
-        res.render("dashboard", {
-          msg: "hi"
-        });
+        res.sendFile(path.join(__dirname, "../public/dashboard.html"));
       });
-    });
+  
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Clients.findOne({ where: { id: req.params.id } }).then(function(dbClients) {
-      res.render("example", {
-        example: dbClients
-      });
-    });
-  });
+  // app.get("/example/:id", function(req, res) {
+  //   db.Clients.findOne({ where: { id: req.params.id } }).then(function(dbClients) {
+  //     res.render("example", {
+  //       example: dbClients
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.render("404");
+    res.sendFile(path.join(__dirname, "../public/404.html"));
   });
 };
